44070ca4b0e501b549716ce8195cf87ab567a1d8
thepanshu <thepanshu.rajput@gmail.com>
2025-10-31 19:00:08 +0530

    theme changing testing

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { useContext } from 'react';
-import { ThemeDataContext } from '../context/ThemeContext';
+import React from "react";
+import { useContext } from "react";
+import { ThemeDataContext } from "../context/ThemeContext";
 
 const Button = () => {
+  const [theme, setTheme] = useContext(ThemeDataContext);
 
-    const [theme, setTheme] = useContext(ThemeDataContext)
+  const changeTheme = () => {
+    setTheme("light");
+  };
+  return (
+    <div>
+      <button onClick={changeTheme}>Change Theme</button>
+    </div>
+  );
+};
 
-    const changeTheme = () => {
-        setTheme('dark')
-    }
-    return (
-        <div >
-            <button onClick={changeTheme}>Change Theme</button>
-        </div>
-    )
-}
-
-export default Button
+export default Button;
