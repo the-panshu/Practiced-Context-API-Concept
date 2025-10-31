a58d56887d141be33495ecb1f7c596c480fecf6d
thepanshu <thepanshu.rajput@gmail.com>
2025-10-31 19:01:32 +0530

    Error resolved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
-import { useContext } from 'react'
-import Nav2 from './Nav2'
-import { ThemeDataContext } from '../context/ThemeContext'
+import { useContext } from "react";
+import Nav2 from "./Nav2";
+import { ThemeDataContext } from "../context/ThemeContext";
 
 const Navbar = () => {
+  const [theme] = useContext(ThemeDataContext);
+  return (
+    <div className={theme}>
+      <h2>Context API Course</h2>
+      <Nav2 />
+    </div>
+  );
+};
 
-    const [theme] = useContext(ThemeDataContext)
-    return (
-        <div className={theme}>
-            <h2>Sheryians</h2>
-            <Nav2 />
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
